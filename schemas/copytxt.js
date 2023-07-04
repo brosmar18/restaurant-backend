32b5e50cb7428ae6fac5e210797b9ef516fd87d9
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-04 14:34:00 -0500

    copytxt schema created

---
+++ schemas/copytxt.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'copyText',
+    title: 'CopyText',
+    type: 'document',
+    fields: [
+        {
+            name: 'title', 
+            title: 'Title', 
+            type: 'string', 
+        }, 
+        {
+            name: 'content', 
+            title: 'Content', 
+            type: 'text',
+        }
+    ]
+};
