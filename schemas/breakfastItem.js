8fb8f883377d07539144b54c35fcdb21cb265f76
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-04 14:55:32 -0500

    breaskItem, and copyText schemas created

---
+++ schemas/breakfastItem.js
@@ -0,0 +1,22 @@
+export default {
+    name: 'breakfastItem',
+    title: 'BreakfastItem',
+    type: 'document',
+    fields: [
+        {
+            name: 'title', 
+            title: 'Title', 
+            type: 'string', 
+        }, 
+        {
+            name: 'desc', 
+            title: 'Desc', 
+            type: 'string',
+        },
+        {
+            name: 'price', 
+            title: 'Price', 
+            type: 'string'
+        }
+    ]
+};
--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import copyText from './copyText';
+import breakfastItem from './breakfastItem';
+
+export const schemaTypes = [
+    copyText,
+    breakfastItem
+]
